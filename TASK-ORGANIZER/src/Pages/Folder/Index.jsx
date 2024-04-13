ab9b24d37c4931f0075dccb6cc906f769f1a96b7
juanRCoder <guillecarck@gmail.com>
2024-04-13 18:16:37 -0500

    feat: submit change

--- TASK-ORGANIZER/src/Pages/Folder/Index.jsx
+++ TASK-ORGANIZER/src/Pages/Folder/Index.jsx
@@ -15,14 +15,13 @@ const List = () => {
 
   return (
     <>
-      <div className='flex w-screen h-screen fixed'>
+      <div className='flex bg-222 w-screen h-screen fixed z'>
         <button onClick={setshowModal} className="bg-black40 flex items-center text-white text-2xl absolute px-8 h-14 ml-5 font-bold rounded-lg top-4 hover:bg-slate-400 ">
           <img className='mx-2 fill-white' src={plus} alt=" plus" />
           Add Folder
         </button>
-        {openModal && <ModalFolder closeModal={closeModal} />}
       </div>
-
+      {openModal && <ModalFolder closeModal={closeModal} />}
     </>
     
   )
