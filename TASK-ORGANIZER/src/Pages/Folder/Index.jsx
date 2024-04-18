a0e750ba2b32e5545f6c024c46a61402834dcb66
OSKAR <omoralespj@gmail.com>
2024-04-18 16:27:03 -0500

    box styles: update the styles of the box

--- TASK-ORGANIZER/src/Pages/Folder/Index.jsx
+++ TASK-ORGANIZER/src/Pages/Folder/Index.jsx
@@ -1,4 +1,4 @@
-import { useContext, useState, useEffect } from "react";
+import { useContext, useState,  } from "react";
 import plus from "../../assets/images/plus.png";
 import ModalFolder from "../Modals/ModalFolder";
 import logo from "../../assets/images/yoshi-removebg-preview.png";
@@ -28,7 +28,7 @@ const List = () => {
 
     // Agregar los datos del contexto al estado local folder para su renderizado
     setFolder((prev) => [...prev, defaultFolder]);
-    [{name, color},{name, color},{name,color}]
+    
 
     // Limpiar los datos del contexto después de renderizar
     setAddFolder({
@@ -39,21 +39,21 @@ const List = () => {
 
   return (
     <>
-      <div className="flex w-screen h-screen fixed bg-gray-200">
+      <div className="flex w-screen h-screen fixed bg-gray-200 items-start">
         <div className="bg-red100 flex items-center justify-center fixed w-full h-24 font-bold">
           <img className="w-14 h-18 left-8 fixed" src={logo} alt="" />
           <h1 className="flex left-40 fixeds text-white text-5xl ">
             Task Organizer
           </h1>
         </div>
-        <div className="pt-5 mt-24 h-full w-full flex">
+        <div className="pt-5 mt-24 h-auto w-auto flex flex-wrap ">
           {/* Agregar dinamicamente los folders */}
           {folder.map((fol, i) => {
             return (
-              <div key={i} className={`${fol.color} h-auto`}>
-                {fol.name}
-                <div>
-                  <button>add task</button>
+              <div  key={i} className={`${fol.color} text-center mx-2 rounded-lg  pt-2 w-80  max-w-52 overflow-hidden break-words` }>
+                <span className="block border-b ">{fol.name}</span>
+                <div className="flex  justify-center pt-2">
+                  <button className="px-2  text-center mx-4 "> <span className="font-bold text-lg">+</span> add task</button>
                 </div>
               </div>
             );
